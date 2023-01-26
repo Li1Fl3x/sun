4639d78a8095a590c9248ce5608d7829fb5cfd51
user2 <user2@example.com>
2023-01-26 13:06:38 +0300

    Добавьте файлы проекта.

---
+++ IPair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sun
+{
+    internal interface IPair
+    {
+       double A { get; set;}   
+        double B { get; set;}
+        IPair Add(IPair p);
+        IPair Sub(IPair p);
+        IPair Mult(IPair p);
+        IPair Div(IPair p);
+        bool Equ(IPair p);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using sun;
+
+Complex g1 = new Complex(1, 2);
+Complex g2 = new Complex(2, 3);
+Complex g3 = (Complex)g1.Add(g2);
+Console.WriteLine(g3);
+Complex g4 = (Complex)g1.Sub(g2);
+Console.WriteLine(g4);
+Complex g5 = (Complex)g1.Mult(g2);
+Console.WriteLine(g5);
+Complex g6 = (Complex)g1.Div(g2);
+Console.WriteLine(g6);
+Console.WriteLine(g1.Equ(g2));
